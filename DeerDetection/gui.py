617cd20da27ca3cd402a5d97c95c57349be82e71
jwlei <joleiros@gmail.com>
2022-04-04 11:14:30 +0200

    Clean up for working  GUI

--- DeerDetection/gui.py
+++ DeerDetection/gui.py
@@ -72,22 +72,6 @@ class GUI:
         btnManualOff["command"] = self.btnManualOff_command
 
 
-        # Capture from camera
-        cap = cv2.VideoCapture(0)
-
-        # function for video streaming
-        def video_stream():
-            _, frame = cap.read()
-            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-            img = Image.fromarray(cv2image)
-            imgtk = ImageTk.PhotoImage(image=img)
-            lmain.imgtk = imgtk
-            lmain.configure(image=imgtk)
-            lmain.after(1, video_stream) 
-
-        video_stream()
-        root.mainloop()
-
     # Button commands
     def btnChangeImgSource_command(self):
         print("Prompt for choosing new image source")
@@ -106,5 +90,5 @@ class GUI:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = App(root)
+    app = GUI(root)
     root.mainloop()
