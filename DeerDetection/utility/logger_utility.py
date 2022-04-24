7c3f889de8d6d5f05d53046d6c8ceb34919ce216
jwlei <joleiros@gmail.com>
2022-04-24 10:18:40 +0200

    Remode misc files

---
+++ DeerDetection/utility/logger_utility.py
@@ -0,0 +1,9 @@
+import logging
+
+class Logger:
+    """ Class to handle logging in application """ 
+    def __init__():
+        
+        # TODO: Implement logging
+
+        return
--- DeerDetection/writeLog.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class Logger:
-    """ Class to handle logging in application """ 
-    def __init__():
-        
-        # TODO: Implement logging
-
-        return
